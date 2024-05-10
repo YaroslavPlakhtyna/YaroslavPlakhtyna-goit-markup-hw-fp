774c21e8b6bbf2886215f408dfc0d0f2536984e3
YaroslavPlakhtyna <plakhtyna.yaroslav@gmail.com>
2024-05-10 23:01:57 +0300

    Final Project

---
+++ src/js/scroll.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  const topElement = document.getElementById("header");
+  const upButton = document.getElementById("up");
+
+  upButton.onclick = () => {
+    topElement.scrollIntoView({ 
+      behavior: 'smooth',
+    })
+  }
+
+  window.addEventListener("scroll", function() {
+    const offsetTop = this.scrollY;
+
+    if (offsetTop > 350) {
+      upButton.classList.add('visible')
+    } else {
+      upButton.classList.remove('visible')
+    }
+  });
+}
